d120a5ed0f062f32c4fdbe863b70329767a1c75f
Ramon-Ventura <ramonh_ventu@hotmail.com>
2020-06-09 15:55:25 -0600

    terminando

--- controller/prestamosController.js
+++ controller/prestamosController.js
@@ -3,5 +3,19 @@ exports.home = (req, res, next) => {
 };
 
 exports.calculoA = (req, res, next) => {
-  console.log(req.body);
+  const { prestamo, anios, interes } = req.body;
+  let meses = parseFloat(anios) * 12;
+  meses = Math.round(meses);
+
+  let _interes = parseFloat(interes) / 100;
+
+  let cuotas = [];
+  let deudas = [];
+  let cuota1 = (prestamo * _interes) / (1 - Math.pow(1 + _interes, -meses));
+  let deuda1 = prestamo - cuota1;
+
+  for (let index = meses; index >= 1; index--) {
+    cuotas.push(prestamo * _interes) / (1 - Math.pow(1 + _interes, -meses));
+  }
+  res.render("calc_tabla", { cuotas });
 };
